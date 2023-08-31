4e69d39bc8ec07046b7f78e8eeb269b4bf07e31d
Hassan Khalil <hkhalil@c4r9p10.1337.ma>
2023-08-31 20:39:28 +0100

    implimenting local strategy

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UsersService){}
+    async validateUser(username: string, pass: string):Promise<any>{
+        const user = await this.userService.findOne(username);
+        if (user && user.password === pass){
+            return result;
+        }
+        return null;
+    }
+}
