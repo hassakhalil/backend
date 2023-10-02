aa368d7e10fece5d3a11a2ab298fca78a1676947
hassan khalil <hassan@hassans-MacBook-Pro-2.local>
2023-10-02 16:01:41 +0100

    added an achievements table to the database

--- backend/prisma/migrations/20231002145742_init/migration.sql
+++ backend/prisma/migrations/20231002145742_init/migration.sql
@@ -26,7 +26,8 @@ CREATE TABLE "games" (
     "id" SERIAL NOT NULL,
     "player_one_id" INTEGER NOT NULL,
     "player_two_id" INTEGER NOT NULL,
-    "game_score" TEXT NOT NULL,
+    "player_one_score" INTEGER NOT NULL,
+    "player_two_score" INTEGER NOT NULL,
     "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "games_pkey" PRIMARY KEY ("id")
@@ -68,16 +69,12 @@ CREATE TABLE "managements" (
 );
 
 -- CreateTable
-CREATE TABLE "leaderboard" (
+CREATE TABLE "achievements" (
     "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
     "user_id" INTEGER NOT NULL,
-    "rank" INTEGER NOT NULL,
-    "level" INTEGER NOT NULL,
-    "wins" INTEGER NOT NULL,
-    "defeats" INTEGER NOT NULL,
-    "games_played" INTEGER NOT NULL,
 
-    CONSTRAINT "leaderboard_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "achievements_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -111,4 +108,4 @@ ALTER TABLE "managements" ADD CONSTRAINT "managements_room_id_fkey" FOREIGN KEY 
 ALTER TABLE "managements" ADD CONSTRAINT "managements_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "leaderboard" ADD CONSTRAINT "leaderboard_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "achievements" ADD CONSTRAINT "achievements_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
