0953a4124c52e072206b6042845a4d43b090a397
hassan khalil <hassan@hassans-MacBook-Pro-2.local>
2023-10-12 14:35:31 +0100

    fixed docker error

--- migrate_run.sh
+++ migrate_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sleep 20
+sleep 10
 # Run Prisma migration without prompting for user input
-npx prisma migrate dev --name init --skip-generate
-
-
+npx prisma generate
+sleep 10
+npx prisma migrate dev --name init
+sleep 5
 npm run start:dev
