fc26207b2a79cce098dc0ba4110f2c21eab3d561
hassan khalil <hassan@hassans-MacBook-Pro-2.local>
2023-10-15 10:23:27 +0100

    added the chat gateway

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
-  providers: [ChatService]
+  providers: [ChatService, ChatGateway]
 })
 export class ChatModule {}
