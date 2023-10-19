b732737ba5bd68e63590401915876fc7ef49293c
hassakhalil <hsnkhalil78@gmail.com>
2023-10-19 18:34:48 +0100

    added the blocked friends list to profile data and fixed the static serve error

--- backend/src/prisma/prisma.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, INestApplication } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
-@Injectable()
-export class PrismaService extends PrismaClient{
-    async onModuleInit() {
-     await this.$connect();   
-    }
-}
--- backend/src/users/dto/tfaCode.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNumberString, IsString, Length } from "class-validator";
-
-export class TfaCodeDto {
-    @IsString()
-    @Length(6, 6)
-    @IsNumberString()
-    code: string;
-}
